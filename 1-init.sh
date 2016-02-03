b97dc636e96cf9ec180e006f4dedac59964683bf
Andrew Holloway <booc0mtaco@gmail.com>
2016-02-03 10:43:08 -0800

    Adding in first batch of configuration files.

---
+++ 1-init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script installs the tools and files necessary to bootstrap the rest of the system
+# things like homebrew and such get installed here. Everything that can be wrapped will 
+# be wrapped, and installed in a shell script named after the wrapper:
+# - all brew installations happen in 2-brew.sh
+# - node packages installed via 2-node.sh
+# - etc.
+# The numbering serves as a mnenonic for the order things get run in
+
+# Install homebrew ! (http://brew.sh)
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Copy configuration files into their proper places
+cp config/.* ~
+
+# Once everything is done, drop a little file in the home directory to say so
+touch ~/.hasrun_init
+
---
+++ 2-brew.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# This file is responsible for setting up the tools and resources via home brew (http://brew.sh)
+# It assumes the init script has already been run, which installs system-wide dependencies and
+# such. Checks for .hasrun_init in home directory.
+
+# homebrew enhancements
+brew tap caskroom/cask     # install desktop apps via command line
+brew tap homebrew/versions # install different versions of commandline tools, etc.
+brew tap caskroom/versions # install different versions of apps
+
+#
+# CLI Section
+#
+
+# command line / shell tools
+brew install ack		    # intelligent text search through files
+brew install tmux		    # replacement for screen. sessions for shells
+brew install tree		    # see the contents of a directory structure
+
+brew install fish		    # alternative CLI shell for the 90s!
+
+# Source Control
+brew install git		    # distributed source control tool
+brew install git-flow		    # methodology for managing branches and development
+brew install tig		    # command line tool for viewing commit history
+
+# development tools
+brew install node		    # all-encompassing JavaScript VM
+
+#
+# GUI / Desktop App Section
+#
+
+# Editors
+brew cask install atom		     # Text editor built on web technologies
+brew cask install cool-retro-term    # make your screen look/feel like an old CRT
+brew cask install macdown	     # edit markdown files and preview them live
+
+# Desktop Tools
+brew cask install caffeine	     # keep your computer awake with one click
+brew cask install spectacle	     # manage keyboard shortcuts to move windows about
+brew cask install quicksilver	     # old-school launcher and manager for system behaviors
+brew cask install xquartz	     # open Source compositing engine 
+brew cask install itsycal	     # system level calendar in a nice little icon
+
+# Finder enhancements
+brew cask install qlmarkdown	     # QuickLook : markdown files
+brew cask install qlstephen	     # Quicklook : text files of different sorts
+brew cask install suspicious-package # Quicklook : view inside zip files before opening them
+
+# Image Tools
+brew cask install imageoptim	     # Optimize JPEG / PNG images
+
+# Programming Tools
+brew cask install gitup		     # Alternative Git GUI
+brew cask install sourcetree	     # Alternative Git GUI
+
+# Browsers
+brew cask install firefox	     # Mozilla browser using Gecko
+brew cask install google-chrome	     # Google browser using Blink

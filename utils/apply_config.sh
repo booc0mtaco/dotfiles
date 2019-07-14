81cb30802d2c918f7b8b9a5bab0522853bc0f7f4
Andrew Holloway <booc0mtaco@gmail.com>
2019-07-14 13:47:46 -0700

    Update scripts for install (Fixes #22)

--- utils/apply_config.sh
+++ utils/apply_config.sh
@@ -7,10 +7,6 @@
 
 CHECKOUT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
 
-echo "*** Running from checkout directory: $CHECKOUT_DIR ($SOURCE_DIR / $DESTINATION_DIR) ***"
-
-echo ${BASH_SOURCE[1]}
-
 # Check to see if we specified a param first
 if [ $# -eq 0 ]; then
 	echo "*** Error: You must specify an existing dotfile to use this script ***"
@@ -19,12 +15,12 @@ fi
 
 # If we did, check to see that the file exists
 if [ ! -f $CHECKOUT_DIR/$SOURCE_DIR/$1 ]; then
-	echo "*** Error: could not find $1 in the config checkout ***"
+	echo "*** Error: could not find $1 in the config checkout: $CHECKOUT_DIR/$SOURCE_DIR/$1 ***"
 	exit 2
 fi
 
 # If it does, check to see if a file exists in the home directory
-echo "*** Symlinking $1 to .config/fish/functions . . . ***"
+echo "*** Symlinking $1 from $CHECKOUT_DIR/$SOURCE_DIR . . . ***"
 
 # if a file exists in the home directory with that name, copy it
 # to a *.bak file.
@@ -39,7 +35,6 @@ fi
 
 # if a symlink already exists, say so, but not an error. Overwrite
 if [ -L $DESTINATION_DIR/$1 ]; then
-	echo "*** Removing old symlink ***"
 	rm $DESTINATION_DIR/$1;
 fi
 
--- utils/apply_fish_functions.sh
+++ utils/apply_fish_functions.sh
@@ -7,7 +7,7 @@
 CHECKOUT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )"
 echo "*** Running from checkout directory: $CHECKOUT_DIR ***"
 
-SOURCE_DIR="config/fish"
-DESTINATION_DIR="$( cd ~/.config/fish/functions && pwd)"
+export SOURCE_DIR="config/fish"
+export DESTINATION_DIR="$( cd ~/.config/fish/functions && pwd)"
 
-$CHECKOUT_DIR/utils/apply_config $1
+$CHECKOUT_DIR/utils/apply_config.sh $1

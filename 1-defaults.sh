55b5ae95f5e872c31846e1236c4baabf832f0608
Andrew Holloway <booc0mtaco@gmail.com>
2020-11-14 19:01:07 -0800

    remove unneeded `sudo` from 1-defaults.sh

--- 1-defaults.sh
+++ 1-defaults.sh
@@ -14,11 +14,6 @@ else
     echo "Begin defaults setup . . ."
 fi
 
-sudo -v
-
-# Sudo keep alive : "update existing `sudo` time stamp until the script has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Expand save panel by default
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true

b5fe051691d2d789cb6f262d71e7aa47f85ed66d
Andrew Holloway <booc0mtaco@users.noreply.github.com>
2024-12-01 18:33:20 -0600

    Add command to run speedtest (via alias)

--- 1-init.sh
+++ 1-init.sh
@@ -68,6 +68,7 @@ $CHECKOUT_DIR/utils/apply_fish_functions.sh fuck.fish
 $CHECKOUT_DIR/utils/apply_fish_functions.sh ack.fish
 $CHECKOUT_DIR/utils/apply_fish_functions.sh top.fish
 $CHECKOUT_DIR/utils/apply_fish_functions.sh tree.fish
+$CHECKOUT_DIR/utils/apply_fish_functions.sh speedtest.fish
 
 # setup for fish shell config - oh-my-fish
 $CHECKOUT_DIR/utils/apply_omf.sh theme
---
+++ config/fish/speedtest.fish
@@ -0,0 +1,3 @@
+function speedtest
+    networkQuality -v
+end
